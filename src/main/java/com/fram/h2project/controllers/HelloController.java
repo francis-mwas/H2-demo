b6d88c2cb350aa53eff21c6d6b5a7d9c6fe147cb
francis-mwas <mwangif184@gmail.com>
2022-03-27 16:40:23 +0300

    Added yml file to manage application properties

--- src/main/java/com/fram/h2project/controllers/HelloController.java
+++ src/main/java/com/fram/h2project/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.fram.h2project.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path= "/api/v1/greetings")
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String message;
+
     @GetMapping
     public String sayHello(){
-        return  "Welcome to js tips website djlkdjljlk";
+        return  message;
     }
 }
